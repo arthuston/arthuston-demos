22c625bb12605ead3cff2a298925ad7a05de23ce
Art Huston <art@arthuston.com>
2018-10-19 17:51:29 -0400

    Initial version of boggle for Java

---
+++ java/boggle/src/com/arthuston/demos/boggle/Boggle.java
@@ -0,0 +1,196 @@
+package com.arthuston.demos.boggle;
+
+import org.apache.commons.lang3.Range;
+
+
+/**
+ * Boggle puzzle game.
+ * Find words in a grid, according to the following rules: The letters must be adjoining in a 'chain'.
+ * Letters in the chain may be adjacent horizontally, vertically, or diagonally. The letter position cannot be used
+ * more than once in the chain.
+ * <p>
+ * This reads the puzzle and list of words from stdin. Example usage:
+ * cat input.txt | python boggle.py
+ * <p>
+ * Sample input:
+ * 5
+ * dogca
+ * ebirt
+ * rxxdm
+ * gesuo
+ * erbil
+ * 6
+ * dog
+ * cat
+ * deer
+ * bird
+ * gerbil
+ * mouse
+ * <p>
+ * The sample input will find dog in the first line, cat in lines 1-2, bird in lines 2-3, mouse in lines 3-4, and gerbil
+ * twice on lines 4-5. deer will not be found.
+ */
+
+import javafx.util.Pair;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+/**
+ * Boggle puzzle game.
+ */
+public class Boggle {
+
+    private final String[] puzzle;
+
+    /**
+     * Constructor.
+     *
+     * @param puzzle the puzzle
+     */
+    public Boggle(String[] puzzle) {
+        this.puzzle = puzzle;
+        // for high-performance these checks would be removed
+        for (String row : puzzle) {
+            if (row.length() != this.puzzle.length) {
+                throw new IllegalArgumentException(String.format("%s is the wrong length, expected %d",
+                        row, this.puzzle.length));
+            }
+        }
+    }
+
+    /**
+     * Solve the puzzle.
+     *
+     * @param words set of words to look for in the puzzle
+     * @return list of words actually found in the puzzle
+     */
+    public List<String> solve(Set<String> words) {
+        List<String> foundWords = new LinkedList<>();
+
+        // look for complete word starting at each character
+        for (int row = 0; row < puzzle.length; row++) {
+            for (int col = 0; col < puzzle.length; col++) {
+                // find words at current position
+                Pair<Integer, Integer> position = position(row, col);
+                Set<Pair<Integer, Integer>> visited = new HashSet<>();
+                String partialWord = "";
+                List<String> newFoundWords = solvePosition(position, visited, partialWord, words);
+
+                // add new found words to list
+                foundWords.addAll(newFoundWords);
+            }
+        }
+
+        return foundWords;
+    }
+
+
+    /**
+     * Solve puzzle recursively at current position.
+     *
+     * @param position      current position
+     * @param visited       previously visited positions
+     * @param partialWord   the partial word
+     * @param matchingWords list of words that match so far
+     * @return list of found words
+     */
+    private List<String> solvePosition(Pair<Integer, Integer> position,
+                                       Set<Pair<Integer, Integer>> visited,
+                                       String partialWord,
+                                       Set<String> matchingWords) {
+        // list of found words is empty,
+        // set this position to visited,
+        // and add character to partial word
+        List<String> foundWords = new LinkedList<>();
+        visited.add(position);
+        char ch = puzzleChar(position);
+        String newPartialWord = new StringBuilder(partialWord).append(ch).toString();
+
+        // check exact match
+        if (matchingWords.contains(newPartialWord)) {
+            foundWords.add(newPartialWord);
+        }
+
+        // check partial words
+        matchingWords = findPartialMatches(newPartialWord, matchingWords);
+
+        // add neighboring characters to the partial word
+        if (matchingWords.size() > 0) {
+            Range<Integer> rowNeighbors = getRowColNeighbors(row(position));
+            Range<Integer> collNeighbors = getRowColNeighbors(col(position));
+            for (int row = rowNeighbors.getMinimum(); row <= rowNeighbors.getMaximum(); row++) {
+                for (int col = collNeighbors.getMinimum(); col <= collNeighbors.getMaximum(); col++) {
+                    Pair nextPosition = position(row, col);
+                    if (!visited.contains(nextPosition)) {
+                        List<String> newFoundWords = solvePosition(nextPosition,
+                                visited, newPartialWord, matchingWords);
+                        foundWords.addAll(newFoundWords);
+                    }
+                }
+            }
+        }
+
+        visited.remove(position);
+        return foundWords;
+    }
+
+    /**
+     * Find partial word matches.
+     *
+     * @param partialWord   partial word
+     * @param matchingWords list of words that match so far
+     * @return set of words where the first len(word) characters match
+     */
+    private Set<String> findPartialMatches(String partialWord, Set<String> matchingWords) {
+        Set<String> newMatchingWords = new HashSet<String>();
+        for (String matchingWord : matchingWords) {
+            if (matchingWord.startsWith(partialWord)) {
+                newMatchingWords.add(matchingWord);
+            }
+        }
+        return newMatchingWords;
+    }
+
+    /**
+     * Get neighbors and self of a row or column
+     *
+     * @param rowCol row or column
+     * @return range row or column neighbors
+     */
+    private Range<Integer> getRowColNeighbors(int rowCol) {
+        // inclusive first row
+        int start = max(rowCol - 1, 0);
+        // inclusive last column
+        int stop = min(rowCol + 1, puzzle.length - 1);
+
+        return Range.between(start, stop);
+    }
+
+    // helper methods that allow us to use Pair as Position
+    Pair<Integer, Integer> position(int row, int col) {
+        return new Pair<>(row, col);
+    }
+
+    int row(Pair<Integer, Integer> position) {
+        return position.getKey();
+    }
+
+    int col(Pair<Integer, Integer> position) {
+        return position.getValue();
+    }
+
+    char puzzleChar(Pair<Integer, Integer> position) {
+        String row = puzzle[row(position)];
+        return row.charAt(col(position));
+    }
+
+
+}
+
+
---
+++ java/boggle/src/com/arthuston/demos/boggle/Main.java
@@ -0,0 +1,86 @@
+/**
+ * Run boggle using standard input.
+ */
+package com.arthuston.demos.boggle;
+
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage: Main <input-file>");
+            System.exit(1);
+        }
+
+        Scanner in = null;
+        try {
+            in = new Scanner(new FileInputStream(args[0]));
+        } catch (Exception e) {
+            System.err.println(e);
+            System.exit(1);
+        }
+
+        String[] puzzle = readPuzzle(in);
+        Set<String> words = readWords(in);
+
+        // solve puzzle
+        Boggle boggle = new Boggle(puzzle);
+        List<String> foundWords = boggle.solve(words);
+
+        // print result
+        System.out.println("Found:");
+        for (String foundWord : foundWords) {
+            System.out.println(foundWord);
+        }
+
+        System.exit(0);
+    }
+
+    /**
+     * Read puzzle from stdin.
+     *
+     * @param in input scanner
+     * @return puzzle
+     */
+    private static String[] readPuzzle(Scanner in) {
+        int puzzleSize = in.nextInt();
+        String[] puzzle = new String[puzzleSize];
+        System.out.println("Puzzle:");
+        for (int i = 0; i < puzzleSize; i++) {
+            puzzle[i] = in.next();
+            System.out.println(puzzle[i]);
+            if (puzzle[i].length() != puzzleSize) {
+                throw new IllegalArgumentException(String.format("%s is the wrong length, expected %d",
+                        puzzle[i], puzzleSize));
+            }
+        }
+        System.out.println();
+        return puzzle;
+    }
+
+    private static Set<String> readWords(Scanner in) {
+        /**
+         * Read set of words from stdin.
+         * @param in input scanner
+         * @return set of words
+         */
+        int wordSize = in.nextInt();
+        Set<String> words = new HashSet<>();
+        System.out.println("Words:");
+        for (int i = 0; i < wordSize; i++) {
+            String word = in.next();
+            words.add(word);
+            System.out.println(word);
+        }
+        System.out.println();
+        return words;
+    }
+}
