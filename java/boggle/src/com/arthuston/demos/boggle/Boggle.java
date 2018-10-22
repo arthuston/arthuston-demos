ec6b1d0f965de0b5d07b7f2198810379df4a7e84
Art Huston <art@arthuston.com>
2018-10-22 09:26:53 -0400

    Minor fixes to java boggle comments and README

--- java/boggle/src/com/arthuston/demos/boggle/Boggle.java
+++ java/boggle/src/com/arthuston/demos/boggle/Boggle.java
@@ -1,8 +1,6 @@
 /**
  * Boggle puzzle game. Find words in a grid, according to the following rules: The letters must be adjoining in a
  * 'chain'. Letters in the chain may be adjacent horizontally, vertically, or diagonally. The letter position cannot be used more than once in the chain.
- *
- * This reads the puzzle and list of words from stdin. Example usage: cat input.txt | python boggle.py
  */
 
 package com.arthuston.demos.boggle;
